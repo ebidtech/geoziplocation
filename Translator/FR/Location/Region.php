2f465fff9847ef93817c5d0d2f6d69ed0bf93f33
Rui Oliveiras <rui96pedro@hotmail.com>
2014-07-01 18:40:42 +0100

    Add Zip codes of France

---
+++ Translator/FR/Location/Region.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * This file is a part of the user agent parser.
+ *
+ * (c) 2013 Ebidtech
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace EBT\GeoZipLocation\Translator\FR\Location;
+
+use EBT\GeoZipLocation\Core\Location\Location;
+use EBT\GeoZipLocation\Core\Repository\LocationRepositoryInterface;
+use EBT\GeoZipLocation\Translator\PT\Repository\RegionRepository;
+
+class Region extends Location
+{
+    const LOCATION_TYPE = 'region';
+
+    /**
+     * Returns Location's Repository
+     *
+     * @return LocationRepositoryInterface
+     */
+    public function getRepository()
+    {
+        return $this->repo;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getType()
+    {
+        return self::LOCATION_TYPE;
+    }
+}
